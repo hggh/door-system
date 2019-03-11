bdc4eb74d4dc67193b9862e430e6d5be3b42227d
Jonas Genannt <jonas@brachium-system.net>
2019-03-11 22:52:49 +0100

    rework to esp8266 12e mqtt sending door status

--- src/main.cpp
+++ src/main.cpp
@@ -1,177 +1,134 @@
 #include <Arduino.h>
-#include <avr/power.h>
+#include <ESP8266WiFi.h>
+#include <ArduinoOTA.h>
 
+#define FASTLED_ESP8266_RAW_PIN_ORDER
 #include <FastLED.h>
-#include <RFM69.h>
-#include <LowPower.h>
+#include <PubSubClient.h>
 #include <Bounce2.h>
-
 
 #include "config.h"
 
-RFM69 radio;
-uint8_t led_brightness = 100;
-volatile short timer_active = 0;
-volatile unsigned short timer_count = 0;
-volatile uint8_t hue = 0;
-volatile boolean light_control = true;
+extern "C" {
+#include "user_interface.h"
+}
+
+#define LED_ON 1
+#define LED_OFF 2
+
+uint8_t outdoor_led_brightness = 100;
+uint8_t indoor_led_brightness = 100;
+volatile uint8_t outdoor_hue = 0;
+CRGB outdoor_leds[OUTDOOR_LED_COUNT];
+volatile short outdoor_timer_active = 0;
+volatile short outdoor_led_status = LED_OFF;
+
 volatile unsigned short door_status = 100;
-volatile short led_display = 1;
-CRGB leds[LED_COUNT];
-Bounce debouncer = Bounce();
+volatile unsigned short led_update = 0;
 
-void leds_clear();
+WiFiClient espClient;
+PubSubClient client(espClient);
+WiFiServer server(80);
+Bounce door = Bounce();
 
+os_timer_t Timer1;
 
-/*
- * ISR should called every second
- */
-ISR(TIMER1_COMPA_vect) {
-  timer_count++;
-
-  fill_rainbow(leds, LED_COUNT, hue++, 5);
-  led_display = 2;
-
-  if (timer_count > 1500) {
-    noInterrupts();
-    leds_clear();
-    TCCR1A = 0;
-    TCCR1B = 0;
-    timer_active = 0;
-    interrupts();
+void outdoor_led_clear() {
+  for (int i = 0; i <= OUTDOOR_LED_COUNT; i++) {
+    outdoor_leds[i] = CRGB(0,0,0);
   }
 }
 
-void setup_timer1() {
-  noInterrupts();
-  TCCR1A = 0;
-  TCCR1B = 0;
-  TCNT1 = 0;
-
-  /*
-   * 1/8000000 * 1024 = .00012800000000000000
-   * 0.02/.000128 - 1= 7813
-   */
-
-  OCR1A = 156;
-  TCCR1B |= _BV(WGM12);
-  TCCR1B |= _BV(CS10);
-  TCCR1B |= _BV(CS12);
-  TIMSK1 |= _BV(OCIE1A);
-  timer_count = 0;
-  timer_active = 1;
-  interrupts();
+void timerCall(void*z) {
+  if (outdoor_led_status == LED_ON) {
+    if (outdoor_timer_active == 0) {
+      outdoor_led_clear();
+      outdoor_led_status = LED_OFF;
+    }
+    else {
+      fill_rainbow(outdoor_leds, OUTDOOR_LED_COUNT, outdoor_hue++, 5);
+      outdoor_timer_active--;
+    }
+    led_update = 1;
+  }
 }
 
-void leds_clear() {
-  FastLED.clear(true);
-  FastLED.show();
+void setup_timer1(void) {
+  os_timer_setfn(&Timer1, timerCall, NULL);
+  os_timer_arm(&Timer1, 1000, true);
+}
+
+void wificonnect() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SECRET_SSID, SECRET_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(200);
+  }
 }
 
 void setup() {
-  pinMode(ALL_LIGHTS_OFF_BUTTON, INPUT_PULLUP);
-  debouncer.attach(ALL_LIGHTS_OFF_BUTTON);
-  debouncer.interval(50);
+  pinMode(DOOR_REED_CONTACT_PIN, INPUT_PULLUP);
+  door.attach(DOOR_REED_CONTACT_PIN);
+  door.interval(250);
 
+  FastLED.addLeds<NEOPIXEL, OUTDOOR_LED_PIN>(outdoor_leds, OUTDOOR_LED_COUNT);
+  FastLED.setBrightness(outdoor_led_brightness);
 
-  pinMode(REED_SWITCH, INPUT);
+  setup_timer1();
+  wificonnect();
 
-  ADCSRA &= ~(1 << 7);
-  power_twi_disable();
-  power_adc_disable();
+  client.setServer(MQTT_SERVER, 1883);
+  client.connect("door", MQTT_USERNAME, MQTT_PASSWORD);
+  server.begin();
 
-  if (DEBUG == 1) {
-    Serial.begin(9600);
-    Serial.println("Starting Door-System");
-  }
-  else {
-    power_usart0_disable();
-  }
-
-  FastLED.addLeds<WS2812, WS2812_PIN>(leds, LED_COUNT);
-  FastLED.setBrightness(led_brightness);
-  leds_clear();
-
-  radio.initialize(FREQUENCY, NODEID, NETWORKID);
-  radio.encrypt(ENCRYPTKEY);
+  outdoor_led_clear();
+  FastLED.show();
 }
 
 void send_door_status(uint8_t door_status) {
-  char buffer[30] = "";
   if (door_status == 1) {
-    sprintf(buffer, "%d;DoorStatus;OPEN", NODEID);
+    client.publish("/door/state", "OPEN");
   }
   else {
-    sprintf(buffer, "%d;DoorStatus;CLOSED", NODEID);
+    client.publish("/door/state", "CLOSED");
   }
-  Serial.println(buffer);
-  radio.sendWithRetry(GATEWAYID, buffer, strlen(buffer), 2);
 }
 
 void loop() {
-  debouncer.update();
-  if ( debouncer.fell() == true ) {
-    char button_all_light_off[30] = "";
-    sprintf(button_all_light_off, "%d;A_LIGHT_OFF", NODEID);
-    radio.sendWithRetry(GATEWAYID, button_all_light_off, strlen(button_all_light_off), 2);
-    Serial.println(button_all_light_off);
+  if (WiFi.status() != WL_CONNECTED) {
+    wificonnect();
   }
-  if (radio.receiveDone()) {
-    String data;
+  ArduinoOTA.handle();
+  door.update();
 
-    for (byte i = 0; i < radio.DATALEN; i++) {
-      data += (char)radio.DATA[i];
-    }
-    uint8_t index = data.indexOf(';');
-    String command = data.substring(0, index);
-
-    if (radio.ACKRequested()) {
-      radio.sendACK();
-    }
-
-    if (DEBUG == 1) {
-      Serial.print('[');
-      Serial.print(radio.SENDERID, DEC);
-      Serial.print("] ");
-      Serial.println(command);
-    }
-
-    if (command.equals("LIGHT")) {
-      setup_timer1();
-      fill_rainbow(leds, LED_COUNT, hue++, 5);
-      FastLED.show();
-    }
-
-    if (command.equals("D_LIGHT")) {
-      light_control = false;
-      if (DEBUG == 1) {
-        Serial.println("Light off");
-      }
-    }
-    
-    if (command.equals("E_LIGHT")) {
-      light_control = true;
-      Serial.println("Light on");
-    }
-  }
-  if (digitalRead(REED_SWITCH) == LOW && light_control == true && timer_active != 1) {
-    Serial.println("Door OPEN but timer was not active");
-    setup_timer1();
-    fill_rainbow(leds, LED_COUNT, hue++, 5);
+  if (led_update == 1) {
     FastLED.show();
+    led_update = 0;
   }
-  if (digitalRead(REED_SWITCH) == HIGH) {
-      if (door_status != 0) {
-      send_door_status(0);
+
+  short current_door_status = door.read();
+
+  // door is opened and door was closed before
+  if (current_door_status == HIGH && door_status != 1) {
+    // enable LEDs first
+    if (outdoor_led_status == LED_OFF) {
+      fill_rainbow(outdoor_leds, OUTDOOR_LED_COUNT, outdoor_hue++, 5);
+      FastLED.show();
+      outdoor_led_status = LED_ON;
+      outdoor_timer_active = 60;
     }
-    door_status = 0;
-  }
-  if (digitalRead(REED_SWITCH) == LOW && door_status != 1) {
+  
+    // send status to OpenHAB2
     send_door_status(1);
     door_status = 1;
   }
-  if (led_display == 2) {
-    FastLED.show();
-    led_display = 0;
+
+  if (current_door_status == LOW && door_status != 0) {
+    send_door_status(0);
+    door_status = 0;
+  }
+
+  if (current_door_status == HIGH) {
+      outdoor_timer_active = 60;
   }
 }
